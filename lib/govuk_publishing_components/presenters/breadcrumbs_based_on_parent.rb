adb7dcc7b0ef62f75a8a36a4de0a85beb00fcae2
Tijmen Brommet <tijmen@gmail.com>
2018-03-27 09:02:35 +0100

    Avoid using the ContentItem class in BreadcrumbsBasedOnParent
    This makes it easier to see what is going on, without having to know
    the internals of `ContentItem`.

--- lib/govuk_publishing_components/presenters/breadcrumbs_based_on_parent.rb
+++ lib/govuk_publishing_components/presenters/breadcrumbs_based_on_parent.rb
@@ -2,12 +2,12 @@ module GovukPublishingComponents
   module Presenters
     class BreadcrumbsBasedOnParent
       def initialize(content_item)
-        @content_item = GovukNavigationHelpers::ContentItem.new(content_item)
+        @content_item = content_item
       end
 
       def breadcrumbs
         ordered_parents = all_parents.map do |parent|
-          { title: parent.title, url: parent.base_path }
+          { title: parent.fetch("title"), url: parent.fetch("base_path") }
         end
 
         ordered_parents << { title: "Home", url: "/" }
@@ -22,11 +22,11 @@ module GovukPublishingComponents
       def all_parents
         parents = []
 
-        direct_parent = content_item.parent
+        direct_parent = content_item.dig("links", "parent", 0)
         while direct_parent
           parents << direct_parent
 
-          direct_parent = direct_parent.parent
+          direct_parent = direct_parent.dig("links", "parent", 0)
         end
 
         parents
